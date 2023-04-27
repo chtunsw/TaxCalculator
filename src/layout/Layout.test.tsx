e8b464989b64fcf975f6529645629d397c3b9e01
Haotian Chen <chtunsw@gmail.com>
2023-04-27 16:28:44 +1000

    move

--- src/layout/Layout.test.tsx
+++ src/layout/Layout.test.tsx
@@ -3,13 +3,13 @@ import { render, screen } from "@testing-library/react";
 import Layout from "./Layout";
 
 describe("Layout", () => {
-  render(
-    <Layout>
-      <h1>Title</h1>
-      <p>content</p>
-    </Layout>
-  );
   it("should display all its children", () => {
+    render(
+      <Layout>
+        <h1>Title</h1>
+        <p>content</p>
+      </Layout>
+    );
     expect(screen.getByText("Title")).toBeInTheDocument();
     expect(screen.getByText("content")).toBeInTheDocument();
   });
